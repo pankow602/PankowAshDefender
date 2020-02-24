acf3aebe21d99f3f75d6a40309ace54aed5e1d50
pankow602 <zzrhydonzz1@gmail.com>
2020-02-24 15:51:11 -0500

    projectile scripting

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    private Transform bullet;
+    public float speed;
+
+    void Start()
+    {
+        bullet = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        bullet.position += Vector3.up * speed;
+        //destroy bullet if passes game border
+        if (bullet.position.y >= 30)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            //increase player score
+        }
+        //if player shoots the base, the base doesn't get damaged
+        else if (other.tag == "base")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
