b5fdf0734c1f63aaf55b08f44bdc60823c749c08
pankow602 <zzrhydonzz1@gmail.com>
2020-02-26 15:47:16 -0500

    player controls are solid. start working on enemy and environment.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -32,9 +32,10 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    void update()
+    void Update()
     {
-        if(Input.GetButton("Fire1") && Time.time> nextFire)
+        //
+        if(Input.GetKeyDown("space") && Time.time> nextFire)
         {
             nextFire = Time.time + fireRate;
             Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
