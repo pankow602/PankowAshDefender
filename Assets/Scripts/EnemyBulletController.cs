442dfeb22a42d3bccf03af4832165f107a3925b8
pankow602 <zzrhydonzz1@gmail.com>
2020-03-01 15:22:44 -0500

    Basic Functions of game complete. Bugs: score does not change, enemy bullets don't seem to move or become stuck.

---
+++ Assets/Scripts/EnemyBulletController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletController : MonoBehaviour
+{
+    private Transform bullet;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(bullet.position.y <= -10){
+            Destroy(bullet.gameObject);
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            GameOver.isPlayerDead = true;
+        }
+        else if(other.tag == "Base")
+        {
+            GameObject playerBase = other.gameObject;
+            BaseHealth baseHealth = playerBase.GetComponent<BaseHealth>();
+            baseHealth.health -= 1;
+            Destroy(gameObject);
+        }
+    }
+}
